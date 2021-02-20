1bb2f1606688f9179e16c374a02093a20f456ce1
D_bxg <ganjinzheng@163.com>
2021-02-20 22:03:13 +0800

    部署懒加载

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,28 @@
 import React, { Component, Fragment, Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from './Home';
-import Detail from './Detail';
+// import Home from './Home';
+// import Detail from './Detail';
 import Nav from "../components/Nav/";
 import Footer from "../components/Footer";
-import Test from "../containers/Test/";
-import Loding from '../components/Loding';
+// import Test from "../containers/Test/";
+import Loding from "../components/Loding";
 
-// const Home = lazy(import('./Home'))
-// const Detail = lazy(import('./Detail'))
+const Home = lazy(() => import("./Home"));
+const Detail = lazy(() => import("./Detail"));
+const Test = lazy(() => import("../containers/Test"));
 
 export default class index extends Component {
   render() {
     return (
       <Fragment>
         <Nav></Nav>
-        {/* <Suspense callback={<Loding></Loding>}> */}
+        <Suspense fallback={<Loding></Loding>}>
           <Switch>
             <Route path="/detail" component={Detail} />
             <Route path="/test" component={Test} />
             <Route path="/" component={Home} />
           </Switch>
-        {/* </Suspense> */}
+        </Suspense>
         {/* <Footer></Footer> */}
       </Fragment>
     );
