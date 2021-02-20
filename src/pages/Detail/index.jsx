2f26dc838cc72b4ac47d31d4c75e47627f20edcb
D_bxg <ganjinzheng@163.com>
2021-02-20 21:37:02 +0800

    临时更改

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import Afp from './afp'
+import Afp from "./afp";
 
 export default class index extends Component {
-    render() {
-        return (
-            <div>
-                <Route path="/detail/afp">
-                    
-                    <Afp></Afp>
-                </Route>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Route path="/detail/afp">
+          <Afp></Afp>
+        </Route>
+      </div>
+    );
+  }
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,27 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./Home";
-import Detail from './Detail'
+import Home from './Home';
+import Detail from './Detail';
 import Nav from "../components/Nav/";
 import Footer from "../components/Footer";
 import Test from "../containers/Test/";
+import Loding from '../components/Loding';
 
+// const Home = lazy(import('./Home'))
+// const Detail = lazy(import('./Detail'))
 
 export default class index extends Component {
   render() {
     return (
       <Fragment>
         <Nav></Nav>
-        <Switch>
-          <Route path="/detail" component={Detail} />
-          <Route path="/test" component={Test}/>
-          <Route path="/" component={Home} />
-        </Switch>
+        {/* <Suspense callback={<Loding></Loding>}> */}
+          <Switch>
+            <Route path="/detail" component={Detail} />
+            <Route path="/test" component={Test} />
+            <Route path="/" component={Home} />
+          </Switch>
+        {/* </Suspense> */}
         {/* <Footer></Footer> */}
       </Fragment>
     );
